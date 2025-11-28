67f89b6a137de07da0513dd6787ff9aeffd67d20
godsofhell <viplavpatel2004@gmail.com>
2025-11-28 18:05:45 +0000

    Fix GitHub Actions workflow to use hardcoded credentials

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_service_plan" "serviceplan" {
   os_type             = var.service_details[2]
 }
 
-resource "azurerm_windows_web_app" "example" {
+resource "azurerm_windows_web_app" "webapp" {
   name                = var.webapp_name
   resource_group_name = var.resource_group_name
   location            = var.location
